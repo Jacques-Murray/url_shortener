efac5d060fb728b33993373797671fe682c214c6
Jacques Murray <jacquesmmurray@gmail.com>
2025-08-07 06:17:43 +0000

    Correct route definition to ensure proper usage of {id} in redirect handler

--- url_shortener/src/routes.rs
+++ url_shortener/src/routes.rs
@@ -9,6 +9,7 @@ use crate::handlers::{redirect, shorten};
 pub fn create_router(pool: SqlitePool) -> Router {
     Router::new()
         .route("/", post(shorten))
+        // This line is now corrected to use {id}
         .route("/:id", get(redirect))
         .with_state(pool)
 }
